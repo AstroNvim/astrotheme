b68c7e6082aa166c605b178517c281d319151ffc
kabinspace <kabinkarki555@gmail.com>
2023-01-26 13:51:20 +0545

    chore(palette): limit the color to 26

--- lua/astrotheme/groups/plugins/indent_blankline.lua
+++ lua/astrotheme/groups/plugins/indent_blankline.lua
@@ -1,7 +1,7 @@
 return {
-  IndentBlanklineSpaceChar = { fg = C.grey_6, nocombine = true },
-  IndentBlanklineChar = { fg = C.grey_6, nocombine = true },
-  IndentBlanklineContextStart = { fg = C.grey_7, underline = true },
-  IndentBlanklineContextChar = { fg = C.grey_7, nocombine = true },
-  IndentBlanklineSpaceCharBlankline = { fg = C.grey_6, nocombine = true },
+  IndentBlanklineSpaceChar = { fg = C.surface1, nocombine = true },
+  IndentBlanklineChar = { fg = C.surface1, nocombine = true },
+  IndentBlanklineContextStart = { fg = C.surface0, underline = true },
+  IndentBlanklineContextChar = { fg = C.surface0, nocombine = true },
+  IndentBlanklineSpaceCharBlankline = { fg = C.surface1, nocombine = true },
 }
