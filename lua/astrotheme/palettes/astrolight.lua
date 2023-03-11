a47fe82b2e7a27db6f0e910a1c2d56120150b025
A-Lamia <git@lamia.io>
2023-03-11 22:42:41 +1100

    feat(theme): add theme astrolight

---
+++ lua/astrotheme/palettes/astrolight.lua
@@ -0,0 +1,72 @@
+local colors = {
+  --- WCAG AA ---
+  red = "#D50B15",
+  blue = "#427080",
+  green = "#547538",
+  yellow = "#965D08",
+  purple = "#B92D98",
+  cyan = "#32765A",
+  orange = "#C63306",
+  -------------------
+  --- WCAG AAA ---
+  -- red = "#A1080F",
+  -- blue = "#325158",
+  -- green = "#3B5629",
+  -- yellow = "#6F4506",
+  -- purple = "#872170",
+  -- cyan = "#235341",
+  -- orange = "#922707",
+  -------------------
+  dark_red = "#F54751",
+  light_blue = "#6FADB5",
+  light_green = "#73A14D",
+  dark_yellow = "#C28A35",
+  light_purple = "#D34BB3",
+  light_cyan = "#5CBA94",
+  dark_orange = "#F9744C",
+  text = "#A5787C",     -- carrot
+  subtext1 = "#C19194", -- headings
+  subtext0 = "#DDACAF",
+  overlay2 = "#F1CBCB",
+  overlay1 = "#E2BEBE", -- status
+  overlay0 = "#FBDADA",
+  surface2 = "#BDA6A1",
+  surface1 = "#D8C1BC",
+  surface0 = "#F5DDD8", -- current line
+  base = "#FFF2EC",
+  mantle = "#FFF2EC", -- inactive
+  crust = "#FFF2EC", -- neo-tree
+  -- icon colors
+  c = "#519aba",
+  css = "#61afef",
+  deb = "#a1b7ee",
+  docker = "#384d54",
+  html = "#de8c92",
+  jpeg = "#c882e7",
+  jpg = "#c882e7",
+  js = "#ebcb8b",
+  jsx = "#519ab8",
+  kt = "#7bc99c",
+  lock = "#c4c720",
+  lua = "#51a0cf",
+  mp3 = "#d39ede",
+  mp4 = "#9ea3de",
+  out = "#abb2bf",
+  png = "#c882e7",
+  py = "#a3b8ef",
+  rb = "#ff75a0",
+  robots = "#abb2bf",
+  rpm = "#fca2aa",
+  rs = "#dea584",
+  toml = "#39bf39",
+  ts = "#519aba",
+  ttf = "#abb2bf",
+  vue = "#7bc99c",
+  woff = "#abb2bf",
+  woff2 = "#abb2bf",
+  zip = "#f9d71c",
+  md = "#519aba",
+  pkg = "#d39ede",
+}
+
+return colors
