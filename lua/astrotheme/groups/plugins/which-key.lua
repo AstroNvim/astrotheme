cf3ff3ab139662064f338a36bd07c5fc3cbda7a5
A-Lamia <git@lamia.io>
2023-04-10 20:06:44 +1000

    feat(which-key): add more hl groups and opts

--- lua/astrotheme/groups/plugins/which-key.lua
+++ lua/astrotheme/groups/plugins/which-key.lua
@@ -1,8 +1,12 @@
-local function callback()
+local function callback(opts)
   return {
-    WhichKeyFloat = { fg = C.fg },
+    WhichKey = { fg = C.purple },
+    WhichKeyFloat = { bg = opts.floating and C.mantle or C.base },
+    WhichKeyBorder = { fg = C.none },
+    WhichKeySeperator = { fg = C.overlay0 },
     WhichKeyDesc = { fg = C.purple },
     WhichKeyGroup = { fg = C.blue },
+    WhichKeyValue = { fg = C.overlay0 },
   }
 end
 
