4e39658e472cdb38efee4c4d6f7e4b86488cdb17
A-Lamia <git@lamia.io>
2023-06-25 23:16:08 +1000

    feat: add dev feature live color reloading
    using dev = true in the settings you can now live reload highlight
    group files and and palette files to see changes reflected on save.

--- lua/astrotheme/init.lua
+++ lua/astrotheme/init.lua
@@ -19,9 +19,9 @@ function M.load(theme)
     "syntax",
     "lsp",
     "astronvim",
-  })
+  }, M.config)
 
-  highlights = util.get_hl_modules(highlights, "astrotheme.groups.plugins", M.config.plugins)
+  highlights = util.get_hl_modules(highlights, "astrotheme.groups.plugins", M.config.plugins, M.config)
 
   util.set_highlights(M.config, highlights, theme)
   if M.config.terminal_colors then util.set_terminal_colors() end
@@ -30,6 +30,23 @@ end
 function M.setup(opts)
   M.config = require("astrotheme.lib.config").user_config(opts)
   M.config.plugins = util.get_plugin_list(M.config)
+
+  if M.config.dev then
+    vim.api.nvim_create_augroup("ThemeUpdate", { clear = true })
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      -- buffer = 0,
+      pattern = "*.lua",
+      group = "ThemeUpdate",
+      callback = function()
+        local theme = vim.g.colors_name
+        if string.match(theme, "astro") then
+          local command = ":colorscheme " .. theme
+          vim.api.nvim_feedkeys(command, "t", true)
+          vim.api.nvim_input "<CR>"
+        end
+      end,
+    })
+  end
 end
 
 return M
