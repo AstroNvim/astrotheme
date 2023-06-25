20edd0ae2c9aef87665746b0a370dc8bad12966b
A-Lamia <git@lamia.io>
2023-06-25 23:22:22 +1000

    feat(neo-tree): make tabs feel more integrated

--- lua/astrotheme/groups/plugins/neo-tree.lua
+++ lua/astrotheme/groups/plugins/neo-tree.lua
@@ -10,22 +10,21 @@ local function callback(opts)
     NeoTreeGitConflict = { fg = C.red },
     NeoTreeGitModified = { fg = C.orange },
     NeoTreeGitUntracked = { fg = C.yellow },
-    NeoTreeNormal = { fg = C.text, bg = opts.transparent and C.none or C.crust },
+    NeoTreeNormal = { fg = C.text, bg = opts.transparent and C.none or C.mantle },
     NeoTreeNormalNC = {
       fg = C.text,
       bg = (opts.transparent and opts.inactive and C.base)
         or (opts.inactive and C.base)
         or (opts.transparent and C.none)
-        or C.crust,
+        or C.mantle,
     },
     NeoTreeSymbolicLinkTarget = { fg = C.cyan },
-
-    NeoTreeTabActive = { fg = C.text, bg = C.base, bold = true },
-    NeoTreeTabInactive = { fg = C.subtext2, bg = C.overlay1 },
-    NeoTreeTabSeparatorActive = { fg = C.text, bg = C.base },
-    NeoTreeTabSeparatorInactive = { fg = C.text, bg = C.overlay1 },
-    NeoTreeVertSplit = { fg = C.base, bg = C.crust },
-    NeoTreeStatusLineNC = { fg = C.crust, bg = C.crust },
+    NeoTreeTabActive = { fg = C.text, bg = C.mantle, bold = true },
+    NeoTreeTabInactive = { fg = C.subtext0, bg = C.crust },
+    NeoTreeTabSeparatorActive = { fg = C.mantle, bg = C.mantle },
+    NeoTreeTabSeparatorInactive = { fg = C.crust, bg = C.crust },
+    NeoTreeVertSplit = { fg = C.base, bg = C.mantle },
+    NeoTreeStatusLineNC = { fg = C.mantle, bg = C.crust },
   }
 end
 
