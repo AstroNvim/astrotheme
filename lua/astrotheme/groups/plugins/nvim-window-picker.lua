26447ae1217a003d89ec06e21b3b3c22c4fe1910
A-Lamia <git@lamia.io>
2023-06-25 23:17:51 +1000

    feat: add nvim-window-picker support

---
+++ lua/astrotheme/groups/plugins/nvim-window-picker.lua
@@ -0,0 +1,10 @@
+local function callback()
+  return {
+    WindowPickerStatusLine = { fg = C.red, bg = C.none },
+    WindowPickerStatusLineNC = { fg = C.red, bg = C.none },
+    WindowPickerWinBar = { fg = C.red, bg = C.none },
+    WindowPickerWinBarNC = { fg = C.red, bg = C.none },
+  }
+end
+
+return callback
