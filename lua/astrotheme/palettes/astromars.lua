5fdd486e121726a64556a77691d67139756472c8
A-Lamia <git@lamia.io>
2023-07-09 04:11:58 +1000

    feat(astromars): initial palette refresh; update to new naming scheme
    astromars is in a bit of a rougher state then astromars
    needs some cleanup and some color changes.

--- lua/astrotheme/palettes/astromars.lua
+++ lua/astrotheme/palettes/astromars.lua
@@ -1,40 +1,83 @@
-local colors = {
+local c = {
   none = "NONE",
-  -------------------
-  red = "#E67C83",
-  blue = "#82A3B0",
-  green = "#7FAA55",
-  yellow = "#CC9300",
-  purple = "#D085BD",
-  cyan = "#57AD8C",
-  orange = "#E08664",
-  -------------------
-  alt_red = "#E77777",
-  alt_blue = "#9CBDC9",
-  alt_green = "#9AC374",
-  alt_yellow = "#FFA31A",
-  alt_purple = "#E8A1D7",
-  alt_cyan = "#88C3AB",
-  alt_orange = "#FAA27F",
-  -------------------
-  base = "#0E0507",
-  crust = "#12070A", -- neo-tree
-  mantle = "#18090E", -- inactive
-  -------------------
-  overlay0 = "#3E222A",
-  overlay1 = "#301B21", -- status
-  overlay2 = "#231418",
-  -------------------
-  surface0 = "#17060A", -- current line
-  surface1 = "#211014",
-  surface2 = "#2E1B20",
-  -------------------
-  text = "#AA989B", -- carrot
-  subtext0 = "#3B2329",
-  subtext1 = "#644B50", -- headings
-  -------------------
-  -- icon colors
-  -------------------
+  syntax = {},
+  ui = {},
+  term = {},
+}
+
+--------------------------------
+--- Syntax
+--------------------------------
+c.syntax.red = "#E67C83"
+c.syntax.blue = "#82A3B0"
+c.syntax.green = "#7FAA55"
+c.syntax.yellow = "#CC9300"
+c.syntax.purple = "#D085BD"
+c.syntax.cyan = "#57AD8C"
+c.syntax.orange = "#E08664"
+c.syntax.text = "#AA989B"
+c.syntax.subtext0 = "#644B50"
+c.syntax.subtext1 = "#3B2329"
+
+--------------------------------
+--- UI
+--------------------------------
+c.ui.red = "#E77777"
+c.ui.blue = "#9CBDC9"
+c.ui.green = "#9AC374"
+c.ui.yellow = "#FFA31A"
+c.ui.purple = "#E8A1D7"
+c.ui.cyan = "#88C3AB"
+c.ui.orange = "#FAA27F"
+
+c.ui.tabline = "#18090E"
+c.ui.winbar = "#644B50"
+c.ui.tool = "#11060B"
+c.ui.base = "#0B0406"
+c.ui.statusline = "#18090E"
+c.ui.split = "#11060B"
+c.ui.popup = "#11060B"
+c.ui.border = "#FAA27F"
+c.ui.current_line = "#17060A"
+c.ui.scrollbar = "#FAA27F"
+c.ui.selection = "#26343F"
+c.ui.highlight = "#242628"
+c.ui.text = "#AA989B"
+
+--------------------------------
+--- terminal
+--------------------------------
+c.term.black = c.ui.tabline
+c.term.bright_black = c.ui.base
+
+c.term.red = c.syntax.red
+c.term.bright_red = c.syntax.red
+
+c.term.green = c.syntax.green
+c.term.bright_green = c.syntax.green
+
+c.term.yellow = c.syntax.yellow
+c.term.bright_yellow = c.syntax.yellow
+
+c.term.blue = c.syntax.blue
+c.term.bright_blue = c.syntax.blue
+
+c.term.purple = c.syntax.purple
+c.term.bright_purple = c.syntax.purple
+
+c.term.cyan = c.syntax.cyan
+c.term.bright_cyan = c.syntax.cyan
+
+c.term.white = c.ui.base
+c.term.bright_white = c.ui.base
+
+c.term.background = c.ui.base
+c.term.foreground = c.ui.text
+
+--------------------------------
+--- Icons
+--------------------------------
+c.icon = {
   c = "#519aba",
   css = "#61afef",
   deb = "#a1b7ee",
@@ -67,4 +110,4 @@ local colors = {
   pkg = "#d39ede",
 }
 
-return colors
+return c
