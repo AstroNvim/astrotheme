9c8848d20a5434101df16942c1b581b45ea99e22
A-Lamia <git@lamia.io>
2023-07-31 19:38:47 +1000

    feat(neo-tree) rework neotree logic and highlight style

--- lua/astrotheme/groups/plugins/neo-tree.lua
+++ lua/astrotheme/groups/plugins/neo-tree.lua
@@ -1,18 +1,36 @@
 local function callback(opts)
   return {
     NeoTreeDirectoryIcon = { fg = C.ui.blue },
-    NeoTreeRootName = { fg = C.ui.text, bold = true },
+    NeoTreeRootName = { fg = C.ui.text_active, bold = true },
     NeoTreeFileName = { fg = C.ui.text },
     NeoTreeFileIcon = { fg = C.ui.text },
     NeoTreeFileNameOpened = { fg = C.ui.green },
-    NeoTreeFloatBorder = { fg = C.ui.border },
-    NeoTreeFloatTitle = { fg = C.ui.title, bg = C.ui.tool },
+    NeoTreeFloatBorder = { fg = C.ui.float },
+    NeoTreeFloatTitle = {
+      fg = opts.title_invert and C.ui.tool or C.ui.title,
+      bg = opts.title_invert and opts.title or C.ui.tool,
+      bold = true,
+    },
     NeoTreeIndentMarker = { fg = C.ui.none_text },
+
     NeoTreeGitAdded = { fg = C.ui.green },
     NeoTreeGitConflict = { fg = C.ui.red },
+    NeoTreeGitDeleted = { fg = C.ui.text_inactive },
+    NeoTreeGitIgnored = { fg = C.ui.text },
     NeoTreeGitModified = { fg = C.ui.orange },
+    NeoTreeGitRenamed = { fg = C.ui.orange },
+    NeoTreeGitStaged = { fg = C.ui.cyan },
     NeoTreeGitUntracked = { fg = C.ui.yellow },
-    NeoTreeTitleBar = { fg = C.ui.tool, bg = C.ui.border },
+    NeoTreeGitUnstaged = { fg = C.ui.yellow },
+
+    NeoTreeTitleBar = {
+      fg = opts.title_invert and C.ui.tool or C.ui.title,
+      bg = (opts.title_invert and C.ui.title)
+        or (opts.neotree and C.ui.tool)
+        or (opts.transparent and C.none)
+        or C.ui.base,
+      bold = true,
+    },
     NeoTreeNormal = {
       fg = C.ui.text,
       bg = (opts.neotree and C.ui.tool) or (opts.transparent and C.none) or C.ui.base,
@@ -32,6 +50,7 @@ local function callback(opts)
     NeoTreeTabSeparatorInactive = { fg = C.ui.tabline, bg = C.ui.tabline },
     NeoTreeVertSplit = { fg = C.ui.split, bg = opts.transparent and C.none or C.ui.base },
     NeoTreeWinSeparator = { fg = C.ui.split, bg = opts.transparent and C.none or C.ui.base, bold = true },
+    NeoTreeCursorLine = { link = "PmenuSel" },
   }
 end
 
