78307775c054dcdd636da3dd3b0599638a53cb6e
A-Lamia <git@lamia.io>
2023-07-31 19:39:33 +1000

    feat(notify): rework logic and highlights

--- lua/astrotheme/groups/plugins/nvim-notify.lua
+++ lua/astrotheme/groups/plugins/nvim-notify.lua
@@ -1,26 +1,36 @@
-local function callback()
+local function callback(opts)
+  local border_bg = (opts.inactive and C.ui.base)
+    or (opts.popup and C.ui.base)
+    or (opts.transparent and C.none)
+    or C.ui.base
+
   return {
-    NotifyBackground = { bg = C.ui.base },
-    NotifyERRORBorder = { fg = C.ui.red },
-    NotifyWARNBorder = { fg = C.ui.orange },
-    NotifyINFOBorder = { fg = C.ui.green },
-    NotifyDEBUGBorder = { fg = C.ui.cyan },
-    NotifyTRACERBorder = { fg = C.ui.purple },
+    NotifyBackground = { link = "NormalPopup" },
+
+    NotifyERRORBorder = { fg = C.ui.red, bg = border_bg },
+    NotifyWARNBorder = { fg = C.ui.orange, bg = border_bg },
+    NotifyINFOBorder = { fg = C.ui.green, bg = border_bg },
+    NotifyDEBUGBorder = { fg = C.ui.cyan, bg = border_bg },
+    NotifyTRACERBorder = { fg = C.ui.purple, bg = border_bg },
+
     NotifyERRORIcon = { fg = C.ui.red },
     NotifyWARNIcon = { fg = C.ui.orange },
     NotifyINFOIcon = { fg = C.ui.green },
     NotifyDEBUGIcon = { fg = C.ui.cyan },
     NotifyTRACEIcon = { fg = C.ui.purple },
+
     NotifyERRORTitle = { fg = C.ui.red },
     NotifyWARNTitle = { fg = C.ui.orange },
     NotifyINFOTitle = { fg = C.ui.green },
     NotifyDEBUGTitle = { fg = C.ui.cyan },
     NotifyTRACETitle = { fg = C.ui.purple },
-    NotifyERRORBody = { fg = C.ui.text },
-    NotifyWARNBody = { fg = C.ui.text },
-    NotifyINFOBody = { fg = C.ui.text },
-    NotifyDEBUGBody = { fg = C.ui.text },
-    NotifyTRACEBody = { fg = C.ui.text },
+
+    NotifyERRORBody = { fg = C.ui.text, bg = border_bg },
+    NotifyWARNBody = { fg = C.ui.text, bg = border_bg },
+    NotifyINFOBody = { fg = C.ui.text, bg = border_bg },
+    NotifyDEBUGBody = { fg = C.ui.text, bg = border_bg },
+    NotifyTRACEBody = { fg = C.ui.text, bg = border_bg },
+
     NotifyLogTime = { fg = C.ui.text_inactive },
     NotifyLogTitle = { fg = C.ui.blue },
   }
