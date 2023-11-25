4d233549aed6ce723e26dc9f513960ba890fa21d
A-Lamia <git@lamia.io>
2023-11-26 00:50:54 +1100

    feat(plugins): add neogit support

---
+++ lua/astrotheme/groups/plugins/neogit.lua
@@ -0,0 +1,33 @@
+local function callback()
+  return {
+    NeogitDiffDelete = {
+      bg = C.ui.base,
+      fg = C.syntax.red,
+    },
+    NeogitDiffDeleteHighlight = {
+      bg = C.ui.base,
+      fg = C.syntax.red,
+    },
+    NeogitDiffAdd = {
+      bg = C.ui.base,
+      fg = C.syntax.green,
+    },
+    NeogitDiffAddHighlight = {
+      bg = C.ui.base,
+      fg = C.syntax.green,
+    },
+    NeogitDiffContextHighlight = {
+      bg = C.ui.inactive_base,
+    },
+    NeogitDiffContext = {
+      bg = C.ui.inactive_base,
+      fg = C.ui.text_inactive,
+    },
+    NeogitHunkHeader = {
+      bg = C.ui.text_inactive,
+      fg = C.ui.text_active,
+    },
+  }
+end
+
+return callback
