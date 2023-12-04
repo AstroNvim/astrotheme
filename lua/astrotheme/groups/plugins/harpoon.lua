dfac7f05807cc66a8fa608741172386a925d11c1
Arthur McLain <mclain.it@gmail.com>
2023-12-04 05:05:14 +0100

    feat: link harpoon highlights to existing groups

--- lua/astrotheme/groups/plugins/harpoon.lua
+++ lua/astrotheme/groups/plugins/harpoon.lua
@@ -1,29 +1,8 @@
-local function callback(opts)
+local function callback()
   return {
-    HarpoonWindow = {
-      fg = C.ui.text,
-      bg = (opts.border and opts.inactive and C.ui.base)
-        or (opts.border and opts.float and C.ui.base)
-        or (opts.inactive and C.ui.prompt)
-        or (opts.float and C.ui.prompt)
-        or (opts.transparent and C.none)
-        or C.ui.base,
-      blend = vim.o.winblend or 0,
-    },
-    HarpoonBorder = {
-      fg = (opts.border and C.ui.border) or (opts.inactive and C.ui.base) or (opts.float and C.ui.prompt) or C.ui.base,
-      bg = (opts.border and opts.inactive and C.ui.base) or (opts.float and C.ui.prompt) or C.ui.prompt,
-      blend = vim.o.winblend or 0,
-    },
-    HarpoonTitle = {
-      fg = (opts.title_invert and C.ui.float) or C.ui.title,
-      bg = (opts.title_invert and C.ui.title)
-        or (opts.float and C.ui.prompt)
-        or (opts.transparent and C.none)
-        or C.ui.base,
-      blend = vim.o.winblend or 0,
-      bold = true,
-    },
+    HarpoonWindow = { link = "NormalFloat" },
+    HarpoonBorder = { link = "FloatBorder" },
+    HarpoonTitle = { link = "FloatTitle" },
   }
 end
 
