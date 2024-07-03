7692d958b5c87236be31246769af8ebe9ec401ca
Micah Halter <micah@mehalter.com>
2024-07-03 11:41:19 -0400

    chore(palettes): setup `icon` table like other tables

--- lua/astrotheme/palettes/astromars.lua
+++ lua/astrotheme/palettes/astromars.lua
@@ -6,6 +6,7 @@ local c = {
   syntax = {},
   ui = {},
   term = {},
+  icon = {},
 }
 
 --------------------------------
@@ -93,37 +94,35 @@ c.term.foreground = c.ui.text
 --------------------------------
 --- Icons
 --------------------------------
-c.icon = {
-  c = "#519aba",
-  css = "#61afef",
-  deb = "#a1b7ee",
-  docker = "#384d54",
-  html = "#de8c92",
-  jpeg = "#c882e7",
-  jpg = "#c882e7",
-  js = "#ebcb8b",
-  jsx = "#519ab8",
-  kt = "#7bc99c",
-  lock = "#c4c720",
-  lua = "#51a0cf",
-  mp3 = "#d39ede",
-  mp4 = "#9ea3de",
-  out = "#abb2bf",
-  png = "#c882e7",
-  py = "#a3b8ef",
-  rb = "#ff75a0",
-  robots = "#abb2bf",
-  rpm = "#fca2aa",
-  rs = "#dea584",
-  toml = "#39bf39",
-  ts = "#519aba",
-  ttf = "#abb2bf",
-  vue = "#7bc99c",
-  woff = "#abb2bf",
-  woff2 = "#abb2bf",
-  zip = "#f9d71c",
-  md = "#519aba",
-  pkg = "#d39ede",
-}
+c.icon.c = "#519aba"
+c.icon.css = "#61afef"
+c.icon.deb = "#a1b7ee"
+c.icon.docker = "#384d54"
+c.icon.html = "#de8c92"
+c.icon.jpeg = "#c882e7"
+c.icon.jpg = "#c882e7"
+c.icon.js = "#ebcb8b"
+c.icon.jsx = "#519ab8"
+c.icon.kt = "#7bc99c"
+c.icon.lock = "#c4c720"
+c.icon.lua = "#51a0cf"
+c.icon.mp3 = "#d39ede"
+c.icon.mp4 = "#9ea3de"
+c.icon.out = "#abb2bf"
+c.icon.png = "#c882e7"
+c.icon.py = "#a3b8ef"
+c.icon.rb = "#ff75a0"
+c.icon.robots = "#abb2bf"
+c.icon.rpm = "#fca2aa"
+c.icon.rs = "#dea584"
+c.icon.toml = "#39bf39"
+c.icon.ts = "#519aba"
+c.icon.ttf = "#abb2bf"
+c.icon.vue = "#7bc99c"
+c.icon.woff = "#abb2bf"
+c.icon.woff2 = "#abb2bf"
+c.icon.zip = "#f9d71c"
+c.icon.md = "#519aba"
+c.icon.pkg = "#d39ede"
 
 return c
