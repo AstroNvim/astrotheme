126c4abfbe6c547668d711af3f15df5d3a4a4332
Micah Halter <micah@mehalter.com>
2024-07-04 15:46:43 -0400

    refactor: refactor development mode and allow multiple colorschemes

--- .lazy.lua
+++ .lazy.lua
@@ -2,105 +2,104 @@
 --- Basically Copy/Paste (with some adjustments) from folke's tokyonight colorscheme:
 --- https://github.com/folke/tokyonight.nvim/blob/c645c1fe778e1428143beca1908b54b78b0b5c1a/.lazy.lua
 
-local colors ---@type AstroThemePalette
-local cache ---@type table<string,string>?
-local hl_groups = {} ---@type { [string]: boolean }
+local M = {
+  module = "astrotheme",
+  colorscheme = "astrotheme",
+  globals = { vim = vim },
+  opts = { plugin_default = true },
+  cache = {}, ---@type table<string, boolean>
+}
+
+function M.reset()
+  local colors = require("astrotheme.lib.util").set_palettes(require("astrotheme").config)
+  M.globals.colors, M.globals.c = colors, colors
+end
 
 local function is_dev()
   local plugin = require("lazy.core.config").spec.plugins["astrotheme"]
   return plugin and require("lazy.core.plugin").values(plugin, "dev")[1]
 end
 
----@param hl AstroThemeHighlight
-local function get_hl_group(hl)
-  local group = "AstroThemeDev" .. vim.inspect(hl):gsub("%W+", "_")
-  if not hl_groups[group] then
-    hl = vim.deepcopy(hl, true)
-    if type(hl) == "string" then hl = { link = hl } end
-    if not hl.fg then hl.fg = colors.syntax.text end
-    vim.api.nvim_set_hl(0, group, hl)
-    hl_groups[group] = true
-  end
-  return group
-end
-
-local function load_highlights()
-  if cache then return end
-  local util = require "astrotheme.lib.util"
-  cache = {}
-  local opts = require("astrotheme").config
-  colors = util.set_palettes(opts)
-  local highlights = util.get_highlights(colors, opts)
-  for k, v in pairs(highlights) do
-    cache[k] = get_hl_group(v)
-  end
-end
-
 local augroup = vim.api.nvim_create_augroup("astrotheme_dev", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePost", {
-  group = augroup,
-  pattern = "*/lua/astrotheme/**.lua",
-  callback = vim.schedule_wrap(function()
-    if is_dev() then
-      local opts = require("astrotheme").config
-      for k in pairs(package.loaded) do
-        if k:find "^astrotheme" then package.loaded[k] = nil end
-      end
-      require("astrotheme").setup(opts)
-      if vim.g.colors_name then vim.cmd.colorscheme(vim.g.colors_name) end
-      hl_groups, cache = {}, nil
-      local hi = require "mini.hipatterns"
-      vim.tbl_map(hi.update, hi.get_enabled_buffers())
-    else
-      vim.api.nvim_del_augroup_by_id(augroup)
+
+local reload = vim.schedule_wrap(function()
+  if is_dev() then
+    for k in pairs(package.loaded) do
+      if k:find("^" .. M.module) then package.loaded[k] = nil end
     end
-  end),
-})
+    require(M.module).setup(M.opts)
+    M.cache = {}
+    local colorscheme = vim.g.colors_name
+    if not colorscheme:match "^astro" then colorscheme = M.colorscheme end
+    vim.cmd.colorscheme(colorscheme)
+    M.reset()
+    local hi = require "mini.hipatterns"
+    vim.tbl_map(hi.update, hi.get_enabled_buffers())
+  else
+    vim.api.nvim_del_augroup_by_id(augroup)
+  end
+end)
+
+vim.api.nvim_create_autocmd("BufWritePost", { group = augroup, pattern = "*/lua/astrotheme/**.lua", callback = reload })
+vim.api.nvim_create_autocmd("User", { group = augroup, pattern = "VeryLazy", callback = reload })
 
 return {
-  "echasnovski/mini.hipatterns",
-  lazy = false,
-  opts = function(_, opts)
-    if not is_dev() then return end
-    opts.highlighters = opts.highlighters or {}
-    opts.highlighters.astrotheme = {
-      pattern = function(buf)
-        local fname = vim.fs.normalize(vim.api.nvim_buf_get_name(buf or 0))
-        if fname:find "lua/astrotheme/groups" and vim.fn.fnamemodify(fname, ":t:r") ~= "init" then
-          load_highlights()
-          return '^%s*%[?"?()[%w%.@]+()"?%]?%s*='
-        end
-      end,
-      group = function(_, match)
-        if cache then return cache[match] end
-      end,
-      extmark_opts = { priority = 2000 },
-    }
+  {
+    "AstroNvim/astroui",
+    optional = true,
+    opts = function(_, opts)
+      if is_dev() then opts.colorscheme = M.colorscheme end
+    end,
+  },
+  {
+    "echasnovski/mini.hipatterns",
+    opts = function(_, opts)
+      local hi = require "mini.hipatterns"
 
-    opts.highlighters.astrotheme_colors = {
-      pattern = {
-        "%f[%w]()c%.[%w_%.]+()%f[%W]",
-        "%f[%w]()colors%.[%w_%.]+()%f[%W]",
-        "%f[%w]()vim%.g%.terminal_color_%d+()%f[%W]",
-      },
-      group = function(_, match)
-        local parts = vim.split(match, ".", { plain = true })
-        local t = _G --[[@as table]]
-        if parts[1]:sub(1, 1) == "c" then
-          table.remove(parts, 1)
-          if not colors then colors = require("astrotheme.lib.util").set_palettes(require("astrotheme").config) end
-          t = colors
-        end
-        local color = vim.tbl_get(t, unpack(parts))
-        return type(color) == "string" and get_hl_group { fg = color }
-      end,
-      extmark_opts = function(_, _, data)
-        return {
-          virt_text = { { "⬤ ", data.hl_group } },
-          virt_text_pos = "inline",
-          priority = 2000,
-        }
-      end,
-    }
-  end,
+      opts.highlighters = opts.highlighters or {}
+
+      opts.highlighters = vim.tbl_extend("keep", opts.highlighters or {}, {
+        hex_color = hi.gen_highlighter.hex_color { priority = 2000 },
+
+        hl_group = {
+          pattern = function(buf)
+            return vim.api.nvim_buf_get_name(buf):find("lua/" .. M.module) and '^%s*%[?"?()[%w%.@]+()"?%]?%s*='
+          end,
+          group = function(_, group)
+            if M.cache[group] == nil then
+              M.cache[group] = false
+              local hl = vim.api.nvim_get_hl(0, { name = group, link = false, create = false })
+              if not vim.tbl_isempty(hl) then
+                hl.fg = hl.fg or vim.api.nvim_get_hl(0, { name = "Normal", link = false }).fg
+                M.cache[group] = true
+                vim.api.nvim_set_hl(0, group .. "Dev", hl)
+              end
+            end
+            return M.cache[group] and group .. "Dev" or nil
+          end,
+          extmark_opts = { priority = 2000 },
+        },
+
+        hl_color = {
+          pattern = {
+            "%f[%w]()c%.[%w_%.]+()%f[%W]",
+            "%f[%w]()colors%.[%w_%.]+()%f[%W]",
+            "%f[%w]()vim%.g%.terminal_color_%d+()%f[%W]",
+          },
+          group = function(_, match)
+            local parts = vim.split(match, ".", { plain = true })
+            local color = vim.tbl_get(M.globals, unpack(parts))
+            return type(color) == "string" and require("mini.hipatterns").compute_hex_color_group(color, "fg")
+          end,
+          extmark_opts = function(_, _, data)
+            return {
+              virt_text = { { "⬤ ", data.hl_group } },
+              virt_text_pos = "inline",
+              priority = 2000,
+            }
+          end,
+        },
+      })
+    end,
+  },
 }
