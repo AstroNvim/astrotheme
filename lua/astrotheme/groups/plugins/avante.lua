4c0d4aa7bdc7fb9f978643f16c61357062a30577
Aaron Pham <Aaronpham0103@gmail.com>
2024-08-27 15:14:08 -0400

    Update lua/astrotheme/groups/plugins/avante.lua
    Co-authored-by: Micah Halter <micah@mehalter.com>

--- lua/astrotheme/groups/plugins/avante.lua
+++ lua/astrotheme/groups/plugins/avante.lua
@@ -1,9 +1,9 @@
 ---@param c AstroThemePalette
 return function(c)
   return {
-    AvanteTitle = { fg = c.ui.green, bg = c.ui.base },
+    AvanteTitle = { fg = c.ui.title, bg = c.ui.base },
     AvanteReversedTitle = { fg = c.ui.base },
-    AvanteSubtitle = { fg = c.ui.blue, bg = c.ui.base },
+    AvanteSubtitle = { fg = c.ui.green, bg = c.ui.base },
     AvanteReversedSubtitle = { fg = c.ui.base },
     AvanteThirdTitle = { fg = c.ui.yellow, bg = c.ui.base },
     AvanteReversedThirdTitle = { fg = c.ui.base },
